ec7450e05dec7657955906185b16733b21fedf18
choushen <jacob.mckenzie@icloud.com>
2024-08-28 01:04:54 +0100

    - Creating folder with extension name for all unique extensions

--- FileSorter/FileSorter.ps1
+++ FileSorter/FileSorter.ps1
@@ -9,3 +9,15 @@ Write-Host "Found $($files.Count) files"
 # Get a list of the extensions
 $list = New-Object System.Collections.ArrayList
 
+foreach($file in $files)
+{
+    Write-Host "Processing $file"
+    $ext = $file.Extension  
+    # Check if folder exists in dir
+    if(-not (Test-Path "$targetDir\$ext"))
+    {
+        Write-Host "Creating folder $ext"
+        New-Item -Path "$targetDir\$ext" -ItemType Directory
+    }
+
+}
