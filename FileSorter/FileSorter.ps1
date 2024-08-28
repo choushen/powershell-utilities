13db06bf69506f5482880accef82ceec608d88e2
choushen <jacob.mckenzie@icloud.com>
2024-08-28 01:05:02 +0100

    Refactor file sorting logic and improve folder creation efficiency

--- FileSorter/FileSorter.ps1
+++ FileSorter/FileSorter.ps1
@@ -1,23 +1,46 @@
+# Properties
+$extList = New-Object System.Collections.ArrayList
+$createdFolderPaths = New-Object System.Collections.ArrayList
+
 # Take path as input
 $targetDir = Read-Host "Enter the path of the directory you want to sort"
 Write-Host "Sorting files in $targetDir"
 
-# Get all files in the directory
-$files = Get-ChildItem -Path $targetDir
+# Get all files in the directory, only files
+$files = Get-ChildItem -Path $targetDir | Where-Object { 
+    -not $_.PSIsContainer -and $_.Extension -notlike "*ps*" 
+}
+
 Write-Host "Found $($files.Count) files"
+foreach ($file in $files) {
+    Write-Host "Processing file: $($file.Name)"
+}
 
-# Get a list of the extensions
-$list = New-Object System.Collections.ArrayList
-
-foreach($file in $files)
+#Create folder for each extension
+for ($i = 0; $i -lt $files.Count; $i++) 
 {
-    Write-Host "Processing $file"
-    $ext = $file.Extension  
-    # Check if folder exists in dir
-    if(-not (Test-Path "$targetDir\$ext"))
-    {
-        Write-Host "Creating folder $ext"
-        New-Item -Path "$targetDir\$ext" -ItemType Directory
-    }
+    # Force extension to lowercase to handle case insensitivity
+    $ext = $files[$i].Extension.Substring(1).ToLower()
 
+    # Check extList does not contain ext and ext does not contain "ps"
+    if((-not $extList.Contains($ext)) -and ($ext -notlike "*ps*"))
+    {
+        $folderPath = "$targetDir\$ext"
+
+        # Create the directory only if it doesn't exist
+        if (-not (Test-Path -Path $folderPath -PathType Container)) {
+            New-Item -ItemType Directory -Path $folderPath
+            $createdFolderPaths.Add($folderPath)
+        } elseif ((Test-Path -Path $folderPath -PathType Container) -and ($createdFolderPaths -notcontains $folderPath)) {
+            $createdFolderPaths.Add($folderPath)
+        }
+
+        # Add the extension to extList to prevent duplicate creation
+        $extList.Add($ext)
+    }
+}
+
+
+foreach ($path in $createdFolderPaths) {
+    Write-Host "Creating folder for $path"
 }
